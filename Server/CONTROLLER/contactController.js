532f439929517db050222e78be5bf7a01935734a
Noufiya <noufiyamn92@gmail.com>
2026-04-28 20:28:24 +0530

    contactpage

--- Server/CONTROLLER/contactController.js
+++ Server/CONTROLLER/contactController.js
@@ -1,21 +1,21 @@
 import Contact from "../MODEL/contactModel.js";
 import { Resend } from "resend";
 
-const resend = new Resend("re_WhFqGFkd_LXTGEQQxKgUx5gU5bixxaVQS");
+import dotenv from "dotenv";
+dotenv.config();
 
-// 🔹 Send Message + Save + Email
-export const sendContact = async (req, res,to) => {
+const resend = new Resend(process.env.RESEND_API_KEY1);
+
+export const sendContact = async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
-    // ✅ Save to DB
     const newMessage = new Contact({ name, email, message });
     await newMessage.save();
 
-    // ✅ Send Email using Resend
     await resend.emails.send({
-      from: "Hi Chicken <onboarding@resend.dev>", // default testing email
-      to: to,
+      from: "Hi Chicken <onboarding@resend.dev>",
+      to: "noufiyamn92@gmail.com", // ✅ FIXED
       subject: `New Message from ${name}`,
       html: `
         <h3>New Contact Message</h3>
@@ -28,7 +28,7 @@ export const sendContact = async (req, res,to) => {
     res.status(200).json({ message: "Message sent successfully" });
 
   } catch (error) {
-    console.error(error);
+    console.error("EMAIL ERROR:", error); // 👈 add this
     res.status(500).json({ message: "Error sending message" });
   }
 };
